1ced79af64c2d5148f72619220adaa074d151cbd
nirvanafan205 <nirvanafan205@gmail.com>
2023-07-27 23:36:13 -0700

    converted css fade in/out to javascript

--- lab.js
+++ lab.js
@@ -1,33 +1,29 @@
 // JavaScript code for the fade-in and fade-out effects
 
-// Function to handle the fade-out effect for lab1
-function fadeOutLab1() {
-  document.getElementById("lab1").style.opacity = "0";
+// Function to fade out element with a given duration
+function fadeOut(element, duration) {
+  element.style.transition = `opacity ${duration}s ease-in-out`;
+  element.style.opacity = 0;
 }
 
-// Function to handle the fade-in effect for lab1
-function fadeInLab1() {
-  document.getElementById("lab1").style.opacity = "1";
+// Function to fade in element with a given duration
+function fadeIn(element, duration) {
+  element.style.transition = `opacity ${duration}s ease-in-out`;
+  element.style.opacity = 1;
 }
 
-// Function to handle the fade-out effect for lab2
-function fadeOutLab2() {
-  document.getElementById("lab2").style.opacity = "0";
-}
+// Get references to the lab1 and lab2 elements
+const lab1 = document.getElementById("lab1");
+const lab2 = document.getElementById("lab2");
 
-// Function to handle the fade-in effect for lab2
-function fadeInLab2() {
-  document.getElementById("lab2").style.opacity = "1";
-}
-
-// Add event listener to lab2 to detect hover
-document.getElementById("lab2").addEventListener("mouseenter", function () {
-  fadeOutLab1();
-  fadeInLab2();
+// Add event listener to lab2 for hover
+lab2.addEventListener("mouseover", () => {
+  fadeIn(lab2, 3); // Fade in lab2 with a duration of 1 second
+  fadeOut(lab1, 1); // Fade out lab1 with a duration of 2 seconds
 });
 
-// Add event listener to lab1 to detect hover
-document.getElementById("lab1").addEventListener("mouseenter", function () {
-  fadeInLab1();
-  fadeOutLab2();
+// Add event listener to lab1 for hover
+lab1.addEventListener("mouseover", () => {
+  fadeIn(lab1, 3); // Fade in lab1 with a duration of 1 second
+  fadeOut(lab2, 1); // Fade out lab2 with a duration of 2 seconds
 });
