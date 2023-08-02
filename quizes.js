aafecb905e30fe5ea3c3e4d30336022e71eae155
nirvanafan205 <nirvanafan205@gmail.com>
2023-08-01 23:20:44 -0700

    fixed quiz

--- quizes.js
+++ quizes.js
@@ -18,24 +18,7 @@ const nextButton = document.getElementById("nextButton");
 // TODO 2: Declare & assign a variable called fact
 // Its value should be an object with a statement, true/false answer, and explanation
 const facts = [
-  {
-    statement:
-      "Which transport layer protocol is the best fit for traffic requiring every packet to be received by the destination, reliably, with nothing lost?",
-    answer: "TCP",
-    explanation:
-      "TCP is a reliable and connection-oriented protocol that ensures every packet is delivered to the destination.",
-    options: ["UDP", "TCP"],
-  },
-
-  {
-    statement:
-      "Which transport layer protocol is the best fit for a live multimedia stream?",
-    answer: "UDP",
-    explanation:
-      "UDP is a connectionless protocol suitable for real-time multimedia streams where some packet loss is acceptable.",
-    options: ["UDP", "TCP"],
-  },
-
+  // question 1
   {
     statement:
       "What part of a network frame contains an 'ACK#' (acknowledgment number), if acknowledging receipt of a packet?",
@@ -51,6 +34,23 @@ const facts = [
     ],
   },
 
+  // question 2
+  {
+    statement:
+      "Which part of a network frame contains an entry indicating how many hops until the packet dies?",
+    answer: "IP Protocol Header",
+    explanation:
+      "The TCP protocol header contains the 'ACK#' field, which is used to acknowledge the receipt of a packet in a network frame.",
+    options: [
+      "Data Payload",
+      "IP Protocol Header",
+      "Frame Header",
+      "TCP Protocol Header",
+      "Frame Trailer",
+    ],
+  },
+
+  // question 4
   {
     statement:
       "Which part of a network frame contains the HTML document returned by a web server (in response to a page request)? ",
@@ -66,6 +66,7 @@ const facts = [
     ],
   },
 
+  // question 5
   {
     statement:
       "Which part of a network frame contains information identifying the destination application (which is listening on the destination computer)?",
@@ -81,6 +82,55 @@ const facts = [
     ],
   },
 
+  // question 6
+  {
+    statement:
+      "Which transport layer protocol is the best fit for traffic requiring every packet to be received by the destination, reliably, with nothing lost?",
+    answer: "TCP",
+    explanation:
+      "TCP is a reliable and connection-oriented protocol that ensures every packet is delivered to the destination.",
+    options: ["UDP", "TCP"],
+  },
+
+  // question 7
+  {
+    statement:
+      "Which transport layer protocol is the best fit for a live multimedia stream?",
+    answer: "UDP",
+    explanation:
+      "UDP is a connectionless protocol suitable for real-time multimedia streams where some packet loss is acceptable.",
+    options: ["UDP", "TCP"],
+  },
+
+  // question 11
+  {
+    statement:
+      "Which of these addresses contains information identifying the manufacturer of a network device?",
+    answer: "0a:00:27:00:00:06",
+    explanation:
+      "The MAC address '0a:00:27:00:00:06' is a Media Access Control address, and the first three bytes (0a:00:27) represent the Organizationally Unique Identifier (OUI), which identifies the manufacturer of the network device.",
+    options: ["0a:00:27:00:00:06", "130.166.42.100", "80"],
+  },
+
+  // question 12
+  {
+    statement:
+      "In a route-able internet address (which contains geographic information in the address itself), the rightmost/least-significant address bits identify the _____.",
+    answer: "Specific host on a given network",
+    explanation:
+      "In a route-able internet address, the rightmost/least-significant address bits identify the specific host on a given network.",
+    options: [
+      "Specific host on a given network",
+      "City",
+      "Geographic region",
+      "Manufacturer",
+      "Serial Number",
+      "Receiving application",
+      "Subnet",
+    ],
+  },
+
+  // question 13
   {
     statement:
       "Which part of a newwork frame is usually comprised of silenece (neigheter 0s nor 1s)?",
@@ -96,9 +146,10 @@ const facts = [
     ],
   },
 
+  // question 14
   {
     statement:
-      "Which part of a newwork frame is usually comprised of silenece (neigheter 0s nor 1s)?",
+      "Which part of a network frame contains the source and destination MAC addresses",
     answer: "Frame Header",
     explanation:
       "The Data Payload contains the actual data being transmitted over the network.",
@@ -111,29 +162,27 @@ const facts = [
     ],
   },
 
-  /*
+  // question 15
   {
     statement:
-      "An HTTP status code of 304",
-    answer: "Frame Header",
+      "An HTTP status code of 304 indicates which of the following conditions...?",
+    answer: "You have the file already (in local cache)",
     explanation:
-      "The Data Payload contains the actual data being transmitted over the network.",
+      "An HTTP status code of 304 (Not Modified) indicates that the requested resource has not been modified since the last time it was accessed and is already cached locally.",
     options: [
-      "Data Payload",
-      "IP Protocol Header",
-      "Frame Header",
-      "TCP Protocol Header",
-      "Frame Trailer",
+      "OK: successful HTTP request",
+      "Requested file not found",
+      "Server crashed",
+      "Server has been hacked",
+      "You have the file already (in local cache)",
     ],
   },
-  */
 ];
 
 let currentFactIndex = 0;
 let currentFact = facts[currentFactIndex];
 
 // TODO 3: Set the text of the statement element to the fact's statement
-
 function displayFact() {
   statement.textContent = currentFact.statement;
 
